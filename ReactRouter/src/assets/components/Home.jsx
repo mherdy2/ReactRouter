1e576c0c1eeb81cc99cb77808cf7557721ed3712
mherdy2 <mherdy_2@hotmail.com>
2023-07-31 22:23:15 -0700

    router

---
+++ ReactRouter/src/assets/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Welcome to my PAGE, please click the color link</h1>
+            </div>
+      );
+}
+ 
+export default Home;
