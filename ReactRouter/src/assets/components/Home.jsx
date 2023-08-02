05845f6d41104aaefc384444c91f36cfb1098776
mherdy2 <mherdy_2@hotmail.com>
2023-08-02 00:38:52 -0700

    navbar

--- ReactRouter/src/assets/components/Home.jsx
+++ ReactRouter/src/assets/components/Home.jsx
@@ -1,8 +1,11 @@
 const Home = () => {
     return (
+        <>
         <div className="home">
-            <h1>Welcome to my PAGE, please click the color link</h1><b/>
-            </div>
+            <h1>Welcome to my PAGE, please click the color link</h1>                 
+            </div>            
+            <div>  </div>          
+     </>            
       );
 }
  
